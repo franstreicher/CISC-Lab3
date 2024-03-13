30adab48f4c51c5958926721d50f93e22dc90140
gabriellabermudez <gbermudez1015@gmail.com>
2024-03-13 19:19:11 -0400

    Update playablecharacterone.gd

--- playablecharacterone.gd
+++ playablecharacterone.gd
@@ -2,11 +2,26 @@ extends Sprite2D
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-if:Input.is_action_just_pressed("ui_right")
-		position.x = + 1
-if:Input.is_action_just_pressed("ui_left")
-		postion.x = postion.x - 1
-if:Input.is_action_just_pressed("ui_up")
-		position.x = position.y + 1
-if:Input.is_action_just_pressed("ui_left")
-		position.x = position.x - 1
+
+const SPEED = 200
+const GRAVITY = 500
+const JUMP_FORCE = -700
+const FLOOR_NORMAL = Vector2(0, -1)
+
+var velocity = Vector2()
+
+func _physics_process(delta):
+	var input_direction = Vector2()
+	input_direction.x = int(Input.is_action_pressed("ui_right")) - int(Input.is_action_pressed("ui_left"))
+	input_direction.y = int(Input.is_action_pressed("ui_down")) - int(Input.is_action_pressed("ui_up"))
+	input_direction = input_direction.normalized()
+	
+	velocity.x = input_direction.x * SPEED
+	velocity.y += GRAVITY * delta
+	
+	if is_on_floor():
+		velocity.y = 0
+		if Input.is_action_just_pressed("ui_jump"):
+			velocity.y = JUMP_FORCE
+	
+	velocity = move_and_slide(velocity, FLOOR_NORMAL)
